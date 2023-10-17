132195677b49f608466053ccc56fbf9ce7b3410c
hangju0610 <phj950610@naver.com>
2023-10-17 20:41:37 +0900

    Feat : swagger config 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Prisma Practices')
+    .setDescription('Prisma API 연습용 페이지')
+    .setVersion('0.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('Docs', app, swaggerDocument);
+
   app.enableCors({
     origin: true,
   });
