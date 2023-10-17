c90fb3fbe9b2b8cb8d6b6439afcd3c4bcbaf446e
hangju0610 <phj950610@naver.com>
2023-10-17 16:27:56 +0900

    Feat : postDto 및 controller 구현

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostService } from './post.service';
+import { Post as PostModel } from '@prisma/client';
+import { PostDto } from './dto/post.dto';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Get()
+  async getAllPosts(): Promise<PostModel[]> {
+    return this.postService.findAllPosts();
+  }
+  @Get(':id')
+  async getPostById(@Param('id') id: number): Promise<PostModel> {
+    return this.postService.findPost(Number(id));
+  }
+  @Post()
+  async createPost(@Body() body: PostDto): Promise<PostModel> {
+    return this.postService.createPost(body);
+  }
+  @Put(':id')
+  async updatePost(
+    @Param('id') id: number,
+    @Body() body: PostDto,
+  ): Promise<PostModel> {
+    return this.postService.updatePost(Number(id), body);
+  }
+
+  @Delete(':id')
+  async deletePost(@Param('id') id: number): Promise<PostModel> {
+    return this.postService.deletePost(Number(id));
+  }
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Post } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
+import { PostDto } from './dto/post.dto';
 
 @Injectable()
 export class PostService {
   constructor(private prisma: PrismaService) {}
 
-  async createPost(data): Promise<Post> {
+  async createPost(data: PostDto): Promise<Post> {
     return this.prisma.post.create({
       data,
     });
   }
-  async updatePost(id: number, data): Promise<Post> {
+  async updatePost(id: number, data: PostDto): Promise<Post> {
     return this.prisma.post.update({
       where: { id },
       data,
