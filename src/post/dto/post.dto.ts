026529f1c7d77764fb5767f785ca535b56c2fcdb
hangju0610 <phj950610@naver.com>
2023-10-17 20:42:59 +0900

    Feat : swagger DTO 설정

--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PostDto {
+  @ApiProperty({
+    nullable: false,
+  })
   title: string;
+
+  @ApiProperty({
+    nullable: false,
+  })
   content: string;
 }
